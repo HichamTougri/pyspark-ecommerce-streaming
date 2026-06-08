9257b751e9d0864fb31ad1ed212d92372c35d639
HichamTougri <106881125+HichamTougri@users.noreply.github.com>
2026-06-08 18:19:20 +0200

    Implement event generation loop in simulateur.py
    Added an infinite loop to generate and log random events in JSON format.

--- simulateur.py
+++ simulateur.py
@@ -17,3 +17,24 @@ print("Démarrage du générateur de flux réseau... (Appuyez sur Ctrl+C pour st
 
 file_counter = 0
 
+
+while True:
+    event = {
+        "timestamp": datetime.datetime.utcnow().isoformat() + "Z",
+        "user_id": f"usr_{random.randint(1000, 9999)}",
+        "user_city": random.choice(cities),
+        "product_id": f"prod_{random.randint(1000, 9999)}",
+        "product_cat": random.choice(categories),
+        "seller_id": f"sel_{random.randint(100, 999)}",
+        "action_type": random.choice(actions),
+        "price": round(random.uniform(10.0, 5000.0), 2)
+    }
+    
+    filename = f"{dossier_flux}/event_{file_counter}.json"
+    with open(filename, 'w') as f:
+        json.dump(event, f)
+        
+    print(f"[LOG] {filename} créé : Action {event['action_type']} - Catégorie {event['product_cat']} - {event['price']}€")
+    
+    file_counter += 1
+    time.sleep(2) 
